407f364cba70d04c6500ebbcfc20960b2d249cb8
Minions <adityawa22@gmail.com>
2022-07-27 13:15:46 +0700

    Point of Sale

---
+++ app/Http/Controllers/ProfileCtrl.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class ProfileCtrl extends Controller
+{
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    function index(){
+        $data = [
+            "title" => "Profile",
+            "page_title" => "Profile",
+            "user" => User::where("id",Auth::user()->id)->first()
+        ];
+
+        return view("profile.frm_profile",$data);
+    }
+
+    function update(Request $req){
+        
+        User::where("id",$req->input("id"))->update([
+            "name"=>$req->input("name"),
+            "email"=>$req->input("email"),
+            "password"=> $req->input("password") == "" ? $req->input("old_password") : Hash::make($req->input("password")),
+        ]);
+
+          // Redirect
+          return redirect('profile');
+    }
+    
+}
